abf36bc8512d8e0633971f8c2cb40a3f900a7eff
Alberto <8672431+albertus82@users.noreply.github.com>
2022-10-20 12:07:05 +0200

    Update table.sql

--- src/main/resources/io/github/albertus82/filestore/table.sql
+++ src/main/resources/io/github/albertus82/filestore/table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE storage (
     filename         VARCHAR(255) NOT NULL PRIMARY KEY,
-    content_length   NUMERIC(19, 0) /* NOT NULL DEFERRABLE INITIALLY DEFERRED */,
+    content_length   NUMERIC(19, 0) /* NOT NULL DEFERRABLE INITIALLY DEFERRED */ CHECK (content_length >= 0),
     last_modified    TIMESTAMP NOT NULL,
     file_contents    BLOB NOT NULL,
     compressed       NUMERIC(1, 0) NOT NULL CHECK (compressed IN (0, 1)),
