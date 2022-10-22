6936647637acee27165881cf56b0635d49b0802e
Alberto <albertus82@users.noreply.github.com>
2022-10-22 12:55:35 +0200

    Renamed getter.

--- src/main/java/io/github/albertus82/filestore/SimpleJdbcFileStore.java
+++ src/main/java/io/github/albertus82/filestore/SimpleJdbcFileStore.java
@@ -58,7 +58,7 @@ public class SimpleJdbcFileStore implements SimpleFileStore {
 		this.blobExtractor = blobExtractor;
 	}
 
-	public String getTableName() {
+	public String getTable() {
 		return table;
 	}
 
