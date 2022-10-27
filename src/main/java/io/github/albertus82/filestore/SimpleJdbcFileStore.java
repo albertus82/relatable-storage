e1a883690c857dbea70b56312e89de68451eac34
Alberto <8672431+albertus82@users.noreply.github.com>
2022-10-27 08:33:54 +0200

    Update SimpleJdbcFileStore.java

--- src/main/java/io/github/albertus82/filestore/SimpleJdbcFileStore.java
+++ src/main/java/io/github/albertus82/filestore/SimpleJdbcFileStore.java
@@ -372,7 +372,7 @@ public class SimpleJdbcFileStore implements SimpleFileStore {
 	private static String bytesToHex(final byte[] bytes) {
 		final byte[] hexChars = new byte[bytes.length * 2];
 		for (int j = 0; j < bytes.length; j++) {
-			int v = bytes[j] & 0xFF;
+			final int v = bytes[j] & 0xFF;
 			hexChars[j * 2] = HEX_ARRAY[v >>> 4];
 			hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
 		}
