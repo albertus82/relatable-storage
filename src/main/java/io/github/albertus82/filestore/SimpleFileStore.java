48738111025d80225f95f877627e0ad69561a5a6
Alberto <albertus82@users.noreply.github.com>
2022-10-28 20:29:27 +0200

    Javadoc.

--- src/main/java/io/github/albertus82/filestore/SimpleFileStore.java
+++ src/main/java/io/github/albertus82/filestore/SimpleFileStore.java
@@ -10,14 +10,63 @@ import org.springframework.core.io.Resource;
 @SuppressWarnings("java:S1130") // Remove the declaration of thrown exception 'java.nio.file.NoSuchFileException' which is a subclass of 'java.io.IOException'. "throws" declarations should not be superfluous (java:S1130)
 public interface SimpleFileStore {
 
+	/**
+	 * Get a list of references to files in the store.
+	 * 
+	 * @param patterns one or more filter patterns that may also include {@code *}
+	 *        and {@code ?} wildcards (optional).
+	 * 
+	 * @return a list of objects referencing the files matching the provided pattern
+	 *         (all files if no pattern is provided)
+	 * 
+	 * @throws IOException if an I/O error occurs
+	 */
 	List<Resource> list(String... patterns) throws IOException;
 
+	/**
+	 * Get a reference to a file in the store.
+	 * 
+	 * @param fileName the name of the desired file (unique key)
+	 * 
+	 * @return an object referencing the requested file
+	 * 
+	 * @throws NoSuchFileException if the specified {@code fileName} does not exist
+	 * @throws IOException if an I/O error occurs
+	 */
 	Resource get(String fileName) throws NoSuchFileException, IOException;
 
+	/**
+	 * Store a new file.
+	 * 
+	 * @param resource the data source of the file
+	 * @param fileName the name of the file to create
+	 *
+	 * @throws FileAlreadyExistsException if a file with the same {@code fileName}
+	 *         already exists
+	 * @throws IOException if an I/O error occurs
+	 */
 	void store(Resource resource, String fileName) throws FileAlreadyExistsException, IOException;
 
+	/**
+	 * Rename a file.
+	 * 
+	 * @param oldFileName the current (old) file name
+	 * @param newFileName the desired (new) file name
+	 * 
+	 * @throws NoSuchFileException if {@code oldFileName} does not exist
+	 * @throws FileAlreadyExistsException if {@code newFileName} already exists
+	 * @throws IOException if an I/O error occurs
+	 */
 	void rename(String oldFileName, String newFileName) throws NoSuchFileException, FileAlreadyExistsException, IOException;
 
+	/**
+	 * Delete a file.
+	 * 
+	 * @param fileName the name of the file (unique key)
+	 *
+	 * @throws NoSuchFileException if {@code fileName} does not exist
+	 * @throws IOException if an I/O error occurs
+	 */
 	void delete(String fileName) throws NoSuchFileException, IOException;
 
 }
