b997a0cac49ba49c8d4abeeb9d6aa8f4b4b49173
Alberto <albertus82@users.noreply.github.com>
2024-05-02 00:17:10 +0200

    UUIDUtils.

--- src/main/java/io/github/albertus82/storage/util/UUIDUtils.java
+++ src/main/java/io/github/albertus82/storage/util/UUIDUtils.java
@@ -1,5 +1,6 @@
 package io.github.albertus82.storage.util;
 
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.nio.LongBuffer;
 import java.util.Base64;
@@ -15,6 +16,10 @@ import java.util.UUID;
  */
 public class UUIDUtils {
 
+	private static final BigInteger B = BigInteger.ONE.shiftLeft(64);
+	private static final BigInteger L = BigInteger.valueOf(Long.MAX_VALUE);
+	private static final BigInteger X = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16);
+
 	private static final Encoder encoder = Base64.getUrlEncoder().withoutPadding();
 	private static final Decoder decoder = Base64.getUrlDecoder();
 
@@ -29,7 +34,7 @@ public class UUIDUtils {
 	 *
 	 * @return A new UUID object constructed using the decoded bytes.
 	 *
-	 * @throws IllegalArgumentException if {@code uuidBase64Url} is not in valid
+	 * @throws IllegalArgumentException if the argument is not in valid
 	 *         <strong>base64url</strong> scheme as defined in RFC 4648.
 	 *
 	 * @see #toBase64Url(UUID)
@@ -56,6 +61,60 @@ public class UUIDUtils {
 	 */
 	public static String toBase64Url(final UUID uuid) {
 		return encoder.encodeToString(ByteBuffer.allocate(Long.BYTES * 2).putLong(uuid.getMostSignificantBits()).putLong(uuid.getLeastSignificantBits()).array());
+	}
+
+	/**
+	 * Converts the specified number into a new UUID object.
+	 * 
+	 * @param num the numeric representation of the UUID, e.g.
+	 *        {@code 281831033846701114428451845565925261418}
+	 * 
+	 * @return A new UUID object based on the integer value.
+	 * 
+	 * @throws IllegalArgumentException if the argument cannot be converted to UUID.
+	 * 
+	 * @see <a href=
+	 *      "https://gist.github.com/drmalex07/9008c611ffde6cb2ef3a2db8668bc251">drmalex07/convertUuidToBigInteger.java
+	 *      - GitHub</a>
+	 */
+	public static UUID fromBigInteger(final BigInteger num) {
+		if (BigInteger.ZERO.compareTo(num) > 0 || X.compareTo(num) < 0) {
+			throw new IllegalArgumentException("0x" + num.toString(16));
+		}
+		final BigInteger[] parts = num.divideAndRemainder(B);
+		BigInteger hi = parts[0];
+		if (L.compareTo(hi) < 0) {
+			hi = hi.subtract(B);
+		}
+		BigInteger lo = parts[1];
+		if (L.compareTo(lo) < 0) {
+			lo = lo.subtract(B);
+		}
+		return new UUID(hi.longValueExact(), lo.longValueExact());
+	}
+
+	/**
+	 * Convert the specified {@link UUID} into a {@link BigInteger}.
+	 * 
+	 * @param uuid the UUID object to convert
+	 * 
+	 * @return A numeric representation of the specified UUID, e.g.
+	 *         {@code 281831033846701114428451845565925261418}
+	 * 
+	 * @see <a href=
+	 *      "https://gist.github.com/drmalex07/9008c611ffde6cb2ef3a2db8668bc251">drmalex07/convertUuidToBigInteger.java
+	 *      - GitHub</a>
+	 */
+	public static BigInteger toBigInteger(final UUID uuid) {
+		BigInteger hi = BigInteger.valueOf(uuid.getMostSignificantBits());
+		if (hi.signum() < 0) {
+			hi = hi.add(B);
+		}
+		BigInteger lo = BigInteger.valueOf(uuid.getLeastSignificantBits());
+		if (lo.signum() < 0) {
+			lo = lo.add(B);
+		}
+		return lo.add(hi.multiply(B));
 	}
 
 }
--- src/test/java/io/github/albertus82/storage/jdbc/UUIDUtilsTest.java
+++ src/test/java/io/github/albertus82/storage/jdbc/UUIDUtilsTest.java
@@ -1,5 +1,6 @@
 package io.github.albertus82.storage.jdbc;
 
+import java.math.BigInteger;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Assertions;
@@ -11,11 +12,26 @@ class UUIDUtilsTest {
 
 	@Test
 	void test() {
-		final UUID a = UUID.randomUUID();
+		BigInteger max = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16);
+		Assertions.assertDoesNotThrow(() -> UUIDUtils.fromBigInteger(BigInteger.ZERO));
+		Assertions.assertDoesNotThrow(() -> UUIDUtils.fromBigInteger(max));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> UUIDUtils.fromBigInteger(max.add(BigInteger.ONE)));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> UUIDUtils.fromBigInteger(BigInteger.ZERO.subtract(BigInteger.ONE)));
+		test(new UUID(0, 0));
+		for (short i = 0; i < Short.MAX_VALUE; i++) {
+			test(UUID.randomUUID());
+		}
+		test(new UUID(0xFFFFFFFFFFFFFFFFL, 0xFFFFFFFFFFFFFFFFL));
+	}
+
+	private static void test(final UUID a) {
 		final String s = UUIDUtils.toBase64Url(a);
 		Assertions.assertEquals(22, s.length());
 		final UUID b = UUIDUtils.fromBase64Url(s);
 		Assertions.assertEquals(a, b);
+		final BigInteger i = UUIDUtils.toBigInteger(b);
+		final UUID c = UUIDUtils.fromBigInteger(i);
+		Assertions.assertEquals(a, c);
 	}
 
 }
